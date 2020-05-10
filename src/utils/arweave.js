898e061cd1d381f370a94b4b797dc4a98ed2fca5
denhampreen <denhampreen@gmail.com>
2020-05-10 16:50:52 +0200

    arweave integration start

--- src/utils/arweave.js
+++ src/utils/arweave.js
@@ -1,6 +1,47 @@
-// To be replaced with an arweave db
-// A bunch of exciting arweave things
 // https://github.com/ArweaveTeam/arweave-js
+
+const Arweave = require("arweave/node");
+
+const arweaveMainetConfig = {
+  host: "arweave.net", // Hostname or IP address for a Arweave host
+  port: 443, // Port
+  protocol: "https", // Network protocol http or https
+  timeout: 20000, // Network request timeouts in milliseconds
+  logging: false,
+};
+
+const arweave = Arweave.init(arweaveMainetConfig);
+
+const createDataTransaction = async (content) => {
+  console.log("store data on arweave");
+  let key = await arweave.wallets.generate();
+
+  let transaction = await arweave.createTransaction(
+    {
+      data: content,
+    },
+    key
+  );
+
+  console.log(transaction);
+};
+
+const getTransaction = (transactionId) =>
+  arweave.transactions.get(transactionId).then((transaction) => {
+    console.log(transaction);
+  });
+
+// const getTransactionEndpoint? = (transactionId) =>
+//   arweave.transactions.get(transactionId).then((transaction) => {
+//     console.log(transaction);
+//   });
+
+const getTransactionData = (transactionId) =>
+  arweave.transactions
+    .getData(transactionId, { decode: true, string: true })
+    .then((data) => {
+      console.log(data);
+    });
 
 const ImmutablePackageNameMapping = (familiarPackageName) => {
   switch (familiarPackageName) {
@@ -11,4 +52,10 @@ const ImmutablePackageNameMapping = (familiarPackageName) => {
   }
 };
 
-export default { ImmutablePackageNameMapping };
+export default {
+  arweave,
+  createDataTransaction,
+  getTransaction,
+  getTransactionData,
+  ImmutablePackageNameMapping,
+};
---
+++ src/utils/db.js
@@ -0,0 +1,4 @@
+// Look up table db for matching transaction hashes to npm file names
+
+// Possible option to add tags
+// https://github.com/ArweaveTeam/arweave-js#add-tags-to-a-transaction
