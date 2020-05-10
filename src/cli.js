253c0b08ae8dac6c20402c5257f16a7a11d50f22
denhampreen <denhampreen@gmail.com>
2020-05-10 09:04:09 +0200

    update cli to es6

---
+++ src/cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import React from "react";
+import { render } from "ink";
+
+import initializeHelp from "./help";
+import Install from "./install.js";
+import Uninstall from "./uninstall.js";
+import Invalid from "./invalid.js";
+
+import Experiment from "./experiment.js";
+
+const [, , ...args] = process.argv;
+
+const arg1 = args[0];
+// const arg2 = args[1];
+
+let command = "";
+
+switch (arg1) {
+  case "install":
+    command = <Install package="{args2}" />;
+    break;
+  case "uninstall":
+    command = <Uninstall package="{args2}" />;
+    break;
+  case "experiment":
+    command = <Experiment />;
+    break;
+  default:
+    command = <Invalid />;
+}
+
+render(command);
