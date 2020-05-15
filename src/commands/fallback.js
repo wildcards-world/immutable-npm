caed4467f123bb9e497c6503fbf0dee87e5b8371
denhampreen <denhampreen@gmail.com>
2020-05-15 23:22:20 +0200

    improve fallback command ux

--- src/commands/fallback.js
+++ src/commands/fallback.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, Color, Box } from "ink";
 import PropTypes from "prop-types";
 import { spawn } from "child_process";
 import Spinner from "ink-spinner";
 
 const Fallback = ({ args }) => {
+  const [state, setState] = useState("");
   useEffect(() => {
+    setState("START");
     let npmFallbackCommand = spawn("npm", args);
 
     npmFallbackCommand.stdout.on("data", function (data) {
       console.log(data.toString());
+      setState("SUCCESS");
     });
 
     npmFallbackCommand.stderr.on("data", function (data) {
@@ -23,16 +26,31 @@ const Fallback = ({ args }) => {
 
   return (
     <>
-      <Box>
-        <Text>
-          <Color yellow>Fallback to native npm command</Color>
-        </Text>
-      </Box>
-      <Box>
-        <Text>
-          <Spinner type="dots" /> Loading...
-        </Text>
-      </Box>
+      {state == "START" && (
+        <>
+          <Box padding={1}>
+            <Text>
+              <Color yellow>Fallback to native npm command</Color>
+            </Text>
+          </Box>
+          <Box>
+            <Text>
+              <Spinner type="dots" /> Loading...
+            </Text>
+          </Box>
+        </>
+      )}
+      {state == "SUCCESS" && (
+        <Box padding={1}>
+          <Color green>Success:</Color>{" "}
+          <Color yellow> inpm {args.join(" ")}</Color>
+        </Box>
+      )}
+      {state == "ERROR" && (
+        <Box padding={1}>
+          <Color red>Error</Color>
+        </Box>
+      )}
     </>
   );
 };
